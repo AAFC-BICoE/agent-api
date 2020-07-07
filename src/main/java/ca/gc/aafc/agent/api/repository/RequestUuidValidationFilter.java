e4dcfd2ad65f9bba930f173237aa272dc304a6ab
Jkeyuk <Jonkero2@gmail.com>
2020-07-07 10:31:47 -0400

    19115_get_with_invalid_uuid_should_return_400
    java docs added

--- src/main/java/ca/gc/aafc/agent/api/repository/RequestUuidValidationFilter.java
+++ src/main/java/ca/gc/aafc/agent/api/repository/RequestUuidValidationFilter.java
@@ -25,6 +25,9 @@ import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
+/**
+ * Filter to intercept HTTP requests and validate the id is a valid UUID.
+ */
 @Log4j2
 public class RequestUuidValidationFilter extends CrnkFilter {
 
@@ -72,11 +75,29 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     super.doFilter(request, response, chain);
   }
 
+  /**
+   * Returns true if the given method is supported by this class. (given method is
+   * present in {@link RequestUuidValidationFilter#METHODS} )
+   * 
+   * @param method
+   *                 - HTTP method to validate
+   * @return
+   */
   private static boolean isValidMethod(String method) {
     return Arrays.asList(METHODS).stream()
         .anyMatch(elemment -> StringUtils.equalsIgnoreCase(method, elemment));
   }
 
+  /**
+   * Removes the pre-set web path prefix from the given uri.
+   * 
+   * @param uri
+   *              - uri to strip
+   * @throws -
+   *             {@link IllegalStateException} if the pre-set web path prefix does
+   *             not match the start of the uri.
+   * @return uri with web path prefix stripped
+   */
   private String stripPrefix(String uri) {
     if (!uri.startsWith(webPathPrefix)) {
       throw new IllegalStateException(
@@ -85,12 +106,27 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     return uri.replaceFirst(webPathPrefix, "");
   }
 
+  /**
+   * Splits a given uri into an array of seperate parts.
+   *
+   * @param uri
+   *              - uri to split
+   * @return array of seperate URI parts
+   */
   private static String[] splitPath(String uri) {
     return Arrays.asList(uri.split(SEPARATOR)).stream()
         .filter(ele -> StringUtils.isNotBlank(ele))
         .toArray(String[]::new);
   }
 
+  /**
+   * Returns the endpoint of the given path if it is present in the pre-set list
+   * of endpoints. Endpoint should be the first element of the given path. (ex.
+   * /{endpoint}/{id}/...)
+   * 
+   * @param path
+   * @return
+   */
   private Optional<String> parseEndpoint(String[] path) {
     if (path.length == 0) {
       return Optional.empty();
@@ -98,6 +134,15 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     return endpoints.stream().filter(end -> StringUtils.equalsIgnoreCase(end, path[0])).findFirst();
   }
 
+  /**
+   * Returns the id part of the given path or Optional empty if one is not
+   * present. ID should be the second element of the path. (ex.
+   * /{endpoint}/{id}/...)
+   *
+   * @param path
+   *               - path to parse
+   * @return
+   */
   private Optional<String> parseUUID(String[] path) {
     if (path.length < 2) {
       return Optional.empty();
@@ -105,6 +150,16 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     return Optional.of(path[1]);
   }
 
+  /**
+   * Sets the given response to an error response for a given uuid and uri.
+   *
+   * @param resp
+   *               - response to set
+   * @param uuid
+   *               - uuid of the request
+   * @param uri
+   *               - uri of the requeest
+   */
   @SneakyThrows
   private static void setErrorResponse(HttpServletResponse resp, String uuid, String uri) {
     resp.setStatus(HttpStatus.BAD_REQUEST_400);
@@ -113,6 +168,16 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     resp.flushBuffer();
   }
 
+  /**
+   * Returns a JSON string representing the body of the error message for a given
+   * uuid and uri.
+   *
+   * @param uuid
+   *               - uuid of the request
+   * @param uri
+   *               - uri of the request
+   * @return JSON string representing the body of the error message
+   */
   @SneakyThrows
   private static String getErrorBody(String uuid, String uri) {
     Map<String, String> metaData = ImmutableMap.of(
@@ -128,6 +193,13 @@ public class RequestUuidValidationFilter extends CrnkFilter {
     return JSON_MAPPER.writeValueAsString(returnBody);
   }
 
+  /**
+   * Returns true if the given string can be parsed as a UUID.
+   *
+   * @param uuid
+   *               - string to validate
+   * @return true if the given string can be parsed as a UUID
+   */
   private static boolean isUuidValid(String uuid) {
     try {
       UUID.fromString(uuid);
