0efd9676cdc90fbff8fe36eb6b64a702cdd8a71a
Jkeyuk <Jonkero2@gmail.com>
2020-10-13 14:47:33 -0400

    20384 org name multi language
    Initial Test setup

---
+++ src/test/java/ca/gc/aafc/agent/api/OrganizationRestIT.java
@@ -0,0 +1,43 @@
+package ca.gc.aafc.agent.api;
+
+import ca.gc.aafc.agent.api.dto.OrganizationDto;
+import ca.gc.aafc.dina.testsupport.BaseRestAssuredTest;
+import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
+import ca.gc.aafc.dina.testsupport.jsonapi.JsonAPITestHelper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest(
+  classes = AgentModuleApiLauncher.class,
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Transactional
+@ContextConfiguration(initializers = {PostgresTestContainerInitializer.class})
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+public class OrganizationRestIT extends BaseRestAssuredTest {
+
+  protected OrganizationRestIT() {
+    super("/api/v1/");
+  }
+
+  @Test
+  void post_maps() {
+    OrganizationDto dto = new OrganizationDto();
+    dto.setName("d");
+
+    String id = super.sendPost(
+      "organization",
+      JsonAPITestHelper.toJsonAPIMap(
+        "organization", JsonAPITestHelper.toAttributeMap(dto), null, null))
+      .extract()
+      .jsonPath()
+      .getString("data.id");
+
+    sendGet("organization", id).log().all(true);
+  }
+
+}
