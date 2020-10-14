a3381462d197fcd99e54373dfcede8ba6f7279d9
Jkeyuk <Jonkero2@gmail.com>
2020-10-14 09:42:43 -0400

    20384 org name multi language
    Update test case post_nameTranslationsPersisted

--- src/test/java/ca/gc/aafc/agent/api/OrganizationRestIT.java
+++ src/test/java/ca/gc/aafc/agent/api/OrganizationRestIT.java
@@ -5,13 +5,17 @@ import ca.gc.aafc.agent.api.entities.OrganizationNameTranslation;
 import ca.gc.aafc.dina.testsupport.BaseRestAssuredTest;
 import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
 import ca.gc.aafc.dina.testsupport.jsonapi.JsonAPITestHelper;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
 
 import javax.transaction.Transactional;
 import java.util.Collections;
+import java.util.Map;
 
 @SpringBootTest(
   classes = AgentModuleApiLauncher.class,
@@ -27,21 +31,47 @@ public class OrganizationRestIT extends BaseRestAssuredTest {
   }
 
   @Test
-  void post_customFieldsResolved() {
+  void post_nameTranslationsPersisted() {
+    OrganizationDto dto = newOrgDTO();
+
+    String id = super.sendPost("organization", mapOrg(dto))
+      .extract().jsonPath().getString("data.id");
+
+    ValidatableResponse response = sendGet("organization", id);
+    response.body("data.attributes.nameTranslations", Matchers.hasSize(1));
+    OrganizationNameTranslation expected = dto.getNameTranslations().get(0);
+    response.body(
+      "data.attributes.nameTranslations[0].language",
+      Matchers.equalTo(expected.getLanguage()));
+    response.body(
+      "data.attributes.nameTranslations[0].value",
+      Matchers.equalTo(expected.getValue()));
+    response.log().all(true);//TODO
+  }
+
+  @Test
+  void delete_nameTranslationsRemoved() {
+    OrganizationDto dto = newOrgDTO();
+
+    String id = super.sendPost("organization", mapOrg(dto))
+      .extract().jsonPath().getString("data.id");
+
+    sendGet("organization", id).log().all(true);//TODO
+  }
+
+  private Map<String, Object> mapOrg(OrganizationDto dto) {
+    return JsonAPITestHelper.toJsonAPIMap(
+      "organization", JsonAPITestHelper.toAttributeMap(dto), null, null);
+  }
+
+  private OrganizationDto newOrgDTO() {
     OrganizationDto dto = new OrganizationDto();
-    dto.setName("d");
-    dto.setNameTranslations(Collections.singletonList(OrganizationNameTranslation.builder()
-      .value("testValue").language("testlang").build()));
-
-    String id = super.sendPost(
-      "organization",
-      JsonAPITestHelper.toJsonAPIMap(
-        "organization", JsonAPITestHelper.toAttributeMap(dto), null, null))
-      .extract()
-      .jsonPath()
-      .getString("data.id");
-
-    sendGet("organization", id).log().all(true);
+    dto.setName(RandomStringUtils.randomAlphabetic(5));
+    OrganizationNameTranslation translation = OrganizationNameTranslation.builder()
+      .value(RandomStringUtils.randomAlphabetic(5))
+      .language(RandomStringUtils.randomAlphabetic(5)).build();
+    dto.setNameTranslations(Collections.singletonList(translation));
+    return dto;
   }
 
 }
