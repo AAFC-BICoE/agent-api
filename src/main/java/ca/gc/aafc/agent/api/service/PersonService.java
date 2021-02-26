f6b69a66beaa95a3133f4dcbe0e680df6972d3e4
Mat Poff <matp@live.ca>
2021-02-26 19:03:32 +0000

    Feedback 21868
    -Added string normalization to PersonService preCreate/preUpdate methods.

--- src/main/java/ca/gc/aafc/agent/api/service/PersonService.java
+++ src/main/java/ca/gc/aafc/agent/api/service/PersonService.java
@@ -3,6 +3,8 @@ package ca.gc.aafc.agent.api.service;
 import java.util.UUID;
 
 import ca.gc.aafc.dina.service.DefaultDinaService;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import ca.gc.aafc.agent.api.entities.Person;
@@ -19,6 +21,20 @@ public class PersonService extends DefaultDinaService<Person> {
   @Override
   protected void preCreate(Person entity) {
     entity.setUuid(UUID.randomUUID());
+    normalizeStrings(entity);
+  }
+
+  @Override
+  protected void preUpdate(Person entity) {
+    normalizeStrings(entity);
+  }
+
+  private void normalizeStrings(Person entity) {
+    entity.setAppellation(StringUtils.normalizeSpace(entity.getAppellation()));
+    entity.setFamilyNames(StringUtils.normalizeSpace(entity.getFamilyNames()));
+    entity.setGivenNames(StringUtils.normalizeSpace(entity.getGivenNames()));
+    entity.setTitle(StringUtils.normalizeSpace(entity.getTitle()));
+    entity.setDisplayName(StringUtils.normalizeSpace(entity.getDisplayName()));
   }
 
 }
