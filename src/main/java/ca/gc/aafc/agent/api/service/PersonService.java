4727cd1dbb7c16881ee4a3fecca043cb20b14a5c
Jkeyuk <jonkero2@gmail.com>
2021-06-08 12:34:14 -0400

    22972 bump dina base 57
    - bump dina base and spring boot
    - adjust service constructors

--- src/main/java/ca/gc/aafc/agent/api/service/PersonService.java
+++ src/main/java/ca/gc/aafc/agent/api/service/PersonService.java
@@ -11,12 +11,13 @@ import org.springframework.stereotype.Service;
 import ca.gc.aafc.agent.api.entities.Person;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import lombok.NonNull;
+import org.springframework.validation.SmartValidator;
 
 @Service
 public class PersonService extends DefaultDinaService<Person> {
 
-  public PersonService(@NonNull BaseDAO baseDAO) {
-    super(baseDAO);
+  public PersonService(@NonNull BaseDAO baseDAO, @NonNull SmartValidator smartValidator) {
+    super(baseDAO, smartValidator);
   }
 
   @Override
