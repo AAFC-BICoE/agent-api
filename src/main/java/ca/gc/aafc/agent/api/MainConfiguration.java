64aa43a1229cb324494252843d7d99497e4aab01
Christian Gendreau <christiangendreau@gmail.com>
2021-08-04 12:21:02 -0400

    23673 Change PersonService to MessageProducingService PersonService can now send events to the message queue. Also provided a fallback implementation to allow the agent-api to run without messaging

--- src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
+++ src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
@@ -1,7 +1,12 @@
 package ca.gc.aafc.agent.api;
 
 import ca.gc.aafc.dina.DinaBaseApiAutoConfiguration;
+import ca.gc.aafc.dina.search.common.config.YAMLConfigProperties;
+import ca.gc.aafc.dina.search.messaging.producer.MessageProducer;
+import ca.gc.aafc.dina.search.messaging.types.DocumentOperationNotification;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,4 +14,23 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackageClasses = DinaBaseApiAutoConfiguration.class)
 @ImportAutoConfiguration(DinaBaseApiAutoConfiguration.class)
 public class MainConfiguration {
+
+  /**
+   * Provides a fallback MessageProducer when messaging.isProducer is not true
+   * To be replaced by LogBasedMessageProducer in search-messaging 0.3
+   */
+  @Configuration
+  public static class FallbackMessageProducer {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MessageProducer init(YAMLConfigProperties yamlConfigProps) {
+      return new MessageProducer(null, yamlConfigProps) {
+        @Override
+        public void send(DocumentOperationNotification documentOperationNotification) {
+         // ignore
+        }
+      };
+    }
+  }
 }
