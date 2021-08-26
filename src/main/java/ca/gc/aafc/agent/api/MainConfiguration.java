a88e72a3b689d9c48337e0d85fa75a064b7d1cc3
Jkeyuk <jonkero2@gmail.com>
2021-08-26 09:53:58 -0400

    23673_change_PersonService_to_MessageProducingService
    adjusted FallbackMessageProducer to initiate when messaging.isProducer = false

--- src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
+++ src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
@@ -5,7 +5,7 @@ import ca.gc.aafc.dina.search.common.config.YAMLConfigProperties;
 import ca.gc.aafc.dina.search.messaging.producer.MessageProducer;
 import ca.gc.aafc.dina.search.messaging.types.DocumentOperationNotification;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +16,14 @@ import org.springframework.context.annotation.Configuration;
 public class MainConfiguration {
 
   /**
-   * Provides a fallback MessageProducer when messaging.isProducer is not true
+   * Provides a fallback MessageProducer when messaging.isProducer is false
    * To be replaced by LogBasedMessageProducer in search-messaging 0.3
    */
   @Configuration
   public static class FallbackMessageProducer {
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnProperty(name = "messaging.isProducer", havingValue = "false")
     public MessageProducer init(YAMLConfigProperties yamlConfigProps) {
       return new MessageProducer(null, yamlConfigProps) {
         @Override
