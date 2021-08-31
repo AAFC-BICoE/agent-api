05b5e9a62d8e0cfca8089da17066fe8d0e745800
Jkeyuk <jonkero2@gmail.com>
2021-08-31 10:27:44 -0400

    23673_change_PersonService_to_MessageProducingService
    remove mapper scan

--- src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
+++ src/main/java/ca/gc/aafc/agent/api/MainConfiguration.java
@@ -3,7 +3,6 @@ package ca.gc.aafc.agent.api;
 import ca.gc.aafc.dina.DinaBaseApiAutoConfiguration;
 import ca.gc.aafc.dina.search.messaging.producer.LogBasedMessageProducer;
 import ca.gc.aafc.dina.search.messaging.producer.MessageProducer;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackageClasses = DinaBaseApiAutoConfiguration.class)
 @ImportAutoConfiguration(DinaBaseApiAutoConfiguration.class)
-@MapperScan(basePackageClasses = DinaBaseApiAutoConfiguration.class)
 public class MainConfiguration {
 
   /**
