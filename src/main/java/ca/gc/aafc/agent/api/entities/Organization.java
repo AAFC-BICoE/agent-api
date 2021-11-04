01791342b77ef6ebf1cc9f0dcc6795d56aae3af0
luusteve <luu.stevenluu@gmail.com>
2021-11-04 14:40:05 -0400

    checkstyle fix

--- src/main/java/ca/gc/aafc/agent/api/entities/Organization.java
+++ src/main/java/ca/gc/aafc/agent/api/entities/Organization.java
@@ -22,7 +22,6 @@ import org.hibernate.annotations.NaturalIdCache;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -30,13 +29,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Entity
 @Getter
--- src/main/java/ca/gc/aafc/agent/api/service/OrganizationService.java
+++ src/main/java/ca/gc/aafc/agent/api/service/OrganizationService.java
@@ -1,22 +1,13 @@
 package ca.gc.aafc.agent.api.service;
 
 import ca.gc.aafc.agent.api.entities.Organization;
-import ca.gc.aafc.agent.api.entities.OrganizationName;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.service.DefaultDinaService;
 import lombok.NonNull;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.SmartValidator;
 
-import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class OrganizationService extends DefaultDinaService<Organization> {
