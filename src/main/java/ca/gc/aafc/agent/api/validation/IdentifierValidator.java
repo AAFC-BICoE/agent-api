44c3f058845c9ef0505f534b75d0e2a3ffa454df
John Phan <johnphan010@gmail.com>
2022-11-15 12:45:47 -0800

    29159_Add_identifier_validator - Added identifier namespace validation

--- src/main/java/ca/gc/aafc/agent/api/validation/IdentifierValidator.java
+++ src/main/java/ca/gc/aafc/agent/api/validation/IdentifierValidator.java
@@ -1,13 +1,32 @@
 package ca.gc.aafc.agent.api.validation;
 
 import ca.gc.aafc.agent.api.entities.Identifier;
+import ca.gc.aafc.agent.api.vocabulary.AgentVocabularyConfiguration;
+import ca.gc.aafc.dina.vocabulary.VocabularyConfiguration.VocabularyElement;
 import lombok.NonNull;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
 
 @Component
 public class IdentifierValidator implements Validator {
+  public static final String IDENTIFIER_NAMESPACE_NOT_IN_VOCABULARY = "validation.constraint.violation.namespaceNotInVocabulary";
+  private final AgentVocabularyConfiguration vocabularyConfiguration;
+  private final MessageSource messageSource;
+
+  public IdentifierValidator(
+    MessageSource messageSource, 
+    AgentVocabularyConfiguration vocabularyConfiguration
+  ) {
+    this.messageSource = messageSource;
+    this.vocabularyConfiguration = vocabularyConfiguration;
+  }
 
   @Override
   public boolean supports(@NonNull Class<?> clazz) {
@@ -19,5 +38,25 @@ public class IdentifierValidator implements Validator {
     if (!supports(target.getClass())) {
       throw new IllegalArgumentException("IdentifierValidator not supported for class " + target.getClass());
     }
+    Identifier identifier = (Identifier) target;
+    validateIdentifierNamespace(errors, identifier);
+  }
+
+  private void validateIdentifierNamespace(Errors errors, Identifier identifier) {
+    if (StringUtils.isNotBlank(identifier.getNamespace())) {
+      Optional<VocabularyElement> foundNamespace = vocabularyConfiguration.getVocabulary()
+          .get("namespace")
+          .stream().filter(o -> o.getName().equalsIgnoreCase(identifier.getNamespace())).findFirst();
+      if (foundNamespace.isPresent()) {
+        identifier.setNamespace(foundNamespace.get().getName());
+      } else {
+        String errorMessage = getMessage(IDENTIFIER_NAMESPACE_NOT_IN_VOCABULARY);
+        errors.rejectValue("namespace", IDENTIFIER_NAMESPACE_NOT_IN_VOCABULARY, errorMessage);
+      }
+    }
+  }
+
+  private String getMessage(String key) {
+    return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
   }
 }
