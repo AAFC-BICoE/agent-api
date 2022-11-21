bafdf7a1f8650683ab57cb0cbb127209152d72e7
Christian Gendreau <christiangendreau@gmail.com>
2022-11-21 18:47:09 -0500

    29313 Handle multiple include requests Added failing test

--- src/test/java/ca/gc/aafc/agent/api/BaseIntegrationTest.java
+++ src/test/java/ca/gc/aafc/agent/api/BaseIntegrationTest.java
@@ -2,14 +2,31 @@ package ca.gc.aafc.agent.api;
 
 import javax.transaction.Transactional;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
 
+import java.util.Properties;
+
 @SpringBootTest(classes = AgentModuleApiLauncher.class)
 @TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
 @Transactional
 @ContextConfiguration(initializers = { PostgresTestContainerInitializer.class })
-public abstract class BaseIntegrationTest { }
+public abstract class BaseIntegrationTest {
+
+  @TestConfiguration
+  public static class CollectionModuleTestConfiguration {
+    @Bean
+    public BuildProperties buildProperties() {
+      Properties props = new Properties();
+      props.setProperty("version", "agent-module-version");
+      return new BuildProperties(props);
+    }
+  }
+
+}
