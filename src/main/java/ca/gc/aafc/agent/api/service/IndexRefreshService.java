72ef8d3ff97b0ac7c2df987b7237e3903021e475
Christian Gendreau <christiangendreau@gmail.com>
2024-12-18 12:00:03 -0500

    35374 Add index refresh endpoint Added new index-refresh resource

---
+++ src/main/java/ca/gc/aafc/agent/api/service/IndexRefreshService.java
@@ -0,0 +1,38 @@
+package ca.gc.aafc.agent.api.service;
+
+import java.util.Set;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import ca.gc.aafc.agent.api.dto.PersonDto;
+import ca.gc.aafc.dina.messaging.message.DocumentOperationNotification;
+import ca.gc.aafc.dina.messaging.message.DocumentOperationType;
+import ca.gc.aafc.dina.messaging.producer.DocumentOperationNotificationMessageProducer;
+
+/**
+ * Service used to send a notification to refresh a document in the search index.
+ */
+@Service
+@ConditionalOnProperty(prefix = "dina.messaging", name = "isProducer", havingValue = "true")
+public class IndexRefreshService {
+
+  private final DocumentOperationNotificationMessageProducer searchRabbitMQMessageProducer;
+  private final Set<String> supportedDocumentTypes;
+
+  public IndexRefreshService(DocumentOperationNotificationMessageProducer searchRabbitMQMessageProducer) {
+    this.searchRabbitMQMessageProducer = searchRabbitMQMessageProducer;
+    // supported document type
+    supportedDocumentTypes = Set.of(PersonDto.TYPENAME);
+  }
+
+  public void reindexDocument(String docId, String type) {
+    if (!supportedDocumentTypes.contains(type)) {
+      throw new IllegalStateException("Unsupported document type");
+    }
+    DocumentOperationNotification don = DocumentOperationNotification.builder()
+      .documentId(docId)
+      .documentType(type)
+      .operationType(DocumentOperationType.UPDATE).build();
+    searchRabbitMQMessageProducer.send(don);
+  }
+}
