9397a0cddafc0dd6a6e69b08146c948db8e5a436
Christian Gendreau <christiangendreau@gmail.com>
2025-03-21 11:08:34 -0400

    Added missing MapStruct scope setting

--- src/main/java/ca/gc/aafc/agent/api/mapper/PersonMapper.java
+++ src/main/java/ca/gc/aafc/agent/api/mapper/PersonMapper.java
@@ -7,7 +7,9 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
+import ca.gc.aafc.agent.api.dto.IdentifierDto;
 import ca.gc.aafc.agent.api.dto.PersonDto;
+import ca.gc.aafc.agent.api.entities.Identifier;
 import ca.gc.aafc.agent.api.entities.Person;
 import ca.gc.aafc.dina.mapper.DinaMapperV2;
 
@@ -24,5 +26,32 @@ public interface PersonMapper extends DinaMapperV2<PersonDto, Person> {
 
   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
   void patchEntity(@MappingTarget Person entity, PersonDto dto, @Context Set<String> provided, @Context String scope);
+
+
+  // --- Relationships handling ---
+  /**
+   * Default method to intercept the mapping and set the context to the relationship
+   * @param dto
+   * @param provided
+   * @param scope will be ignored but required so MapStruct uses it
+   * @return
+   */
+  default Identifier toEntity(IdentifierDto dto, @Context Set<String> provided, @Context String scope) {
+    return toIdentifierEntity(dto, provided, "identifiers");
+  }
+
+  /**
+   * Default method to intercept the mapping and set the context to the relationship
+   * @param dto
+   * @param provided
+   * @param scope will be ignored but required so MapStruct uses it
+   * @return
+   */
+  default IdentifierDto toDto(Identifier dto, @Context Set<String> provided, @Context String scope) {
+    return toIdentifierDto(dto, provided, "identifiers");
+  }
+
+  Identifier toIdentifierEntity(IdentifierDto dto, Set<String> provided, String scope);
+  IdentifierDto toIdentifierDto(Identifier entity, Set<String> provided, String scope);
 
 }
