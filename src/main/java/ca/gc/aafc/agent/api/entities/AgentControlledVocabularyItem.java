d581432d1ed46ba4842aa82191b559dd232f946d
Christian Gendreau <christiangendreau@gmail.com>
2026-09-11 08:36:34 -0400

    Added entity and dto

---
+++ src/main/java/ca/gc/aafc/agent/api/entities/AgentControlledVocabularyItem.java
@@ -0,0 +1,25 @@
+package ca.gc.aafc.agent.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import ca.gc.aafc.dina.entity.ControlledVocabularyItem;
+
+@Entity(name = "controlled_vocabulary_item")
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+public class AgentControlledVocabularyItem extends ControlledVocabularyItem {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = CONTROLLED_VOCABULARY_COL_NAME)
+  private AgentControlledVocabulary controlledVocabulary;
+
+}
